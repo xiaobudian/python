f12445f74880f973d72a4e3cde5987b39d327f62
liushuai <849351660@qq.com>
2015-11-03 19:15:34 +0800

    create area.xml

--- python/ls/pm25/views.py
+++ python/ls/pm25/views.py
@@ -119,19 +119,16 @@ def getareaxml(request):
         
         for p in Province.objects.all():
             province = doc.createElement('province')
-            province.setAttribute('cid', p.code)
-            province.setAttribute('n', p.name)
-            province.setAttribute('pid', p.parent)
+            province.setAttribute('name', p.name)
+            province.setAttribute('code', p.code)
             for c in City.objects.filter(parent=p.code):
                 city = doc.createElement('city')
-                city.setAttribute('cid', c.code)
-                city.setAttribute('n', c.name)
-                city.setAttribute('pid', c.parent)
+                city.setAttribute('name', c.name)
+                city.setAttribute('code', c.code)
                 for t in Town.objects.filter(parent=c.code):
                     town = doc.createElement('town')
-                    town.setAttribute('cid', t.code)
-                    town.setAttribute('n', t.name)
-                    town.setAttribute('pid', t.parent)
+                    town.setAttribute('name', t.name)
+                    town.setAttribute('code', t.code)
                     city.appendChild(town)
                 province.appendChild(city)
             body.appendChild(province)
